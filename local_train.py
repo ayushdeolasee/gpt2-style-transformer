e560cb03caa8edb8ba7b553233efc1ae12249ecb
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-07-16 13:29:15 +0530

    adding training code

--- local_train.py
+++ local_train.py
@@ -1,30 +1,47 @@
 import torch.backends
 import torch.nn as nn
+import inspect
 import torch
 import torch.nn.functional as F
 import numpy as np
 import math
-import os
-import logging
-import inspect
-from rich import print
-from rich.logging import RichHandler
-import click
+import os 
 
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
-
-log = logging.getLogger("rich")
+# Train parameters
+# vocab_size = 50304
+# block_size = 1024 
+# total_batch_size = 524288
+# batch_size = 8
+# weight_decay = 0.01
+# epochs = 19073
+# lr = 3e-4
+# max_steps = epochs
+# warmup_steps = 750
+# max_lr = lr
+# min_lr = max_lr * 0.1
 
 device = torch.device("cpu")
 if torch.cuda.is_available():
     device = "cuda"
 elif torch.backends.mps.is_available():
     device = "mps" 
+dataset_location = "fineweb10B"
+print(f"Using device: {device}")
+vocab_size = 50304 
+weight_decay = 0.1
+block_size = 1024
+batch_size = 4 
+epochs = 19073
+total_batch_size = 524288
+lr = 3e-4
+max_steps = epochs
+warmup_steps = 150
+max_lr = lr
+min_lr = max_lr * 0.1
 
-print(f"[green]Using device: {device}[/green] :smile: ")
+assert total_batch_size % (batch_size * block_size) == 0, "total_batch_size must be divisble by B * T"
+grad_accum_steps = total_batch_size // (batch_size * block_size)
+print(grad_accum_steps)
 
 def load_tokens(filename):
     npt = np.load(filename)
@@ -32,23 +49,20 @@ def load_tokens(filename):
     ptt = torch.tensor(npt, dtype=torch.long)
     return ptt
 
-
 class DataLoaderLite:
-    def __init__(self, B, T, split, data_root):
+    def __init__(self, B, T, split):
         self.B = B
         self.T = T
         assert split in {'train', 'val'}
 
-        data_root = data_root
+        data_root = dataset_location
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
         shards = sorted(shards)
         shards = [os.path.join(data_root, s) for s in shards]
         self.shards = shards
-        if (len(shards) > 0) == False:
-            log.error(f"no shards found for split {split}")
-        # assert len(shards) > 0, f"no shards found for split {split}"
-        print(f"[green]found {len(shards)} shards for split {split}[/green]")
+        assert len(shards) > 0, f"no shards found for split {split}"
+        print(f"found {len(shards)} shards for split {split}")
         self.reset()
 
     def reset(self):
@@ -87,9 +101,9 @@ class RMSNorm(nn.Module):
 # TODO: Review implementation of CausalSelfAttention
 class CausalSelfAttention(nn.Module):
     def __init__(self, n_embed, n_head):
-        super().__init__()
         self.n_embd = n_embed
         self.n_head = n_head
+        super().__init__()
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(self.n_embd, 3 * self.n_embd)
         # output projection
@@ -126,19 +140,19 @@ class MLP(nn.Module):
         return self.MLP(x)
 
 class Block(nn.Module):
-    def __init__(self, embed_dim, num_heads):
+    def __init__(self, embed_dim, num_heads, dropout):
         super(Block, self).__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
 
         self.rmsnorm1 = RMSNorm(embed_dim) 
         self.MultiheadAttention = CausalSelfAttention(self.embed_dim, self.num_heads)
-        self.rmsnrom2 = RMSNorm(embed_dim)
+        self.rmsnorm2 = RMSNorm(embed_dim)
         self.MLP = MLP(embed_dim)
         
     def forward(self, x):
         x = x + self.MultiheadAttention(self.rmsnorm1(x))
-        x = x + self.MLP(self.rmsnrom2(x))
+        x = x + self.MLP(self.rmsnorm2(x))
         return x
 
 class Model1(nn.Module):
@@ -156,7 +170,7 @@ class Model1(nn.Module):
 
         self.blocks = nn.ModuleList([Block(self.embed_dim, self.num_heads, self.dropout) for _ in range(self.num_blocks)])
 
-        self.rmsnrom3 = RMSNorm(self.embed_dim)
+        self.rmsnorm3 = RMSNorm(self.embed_dim)
         self.lm_linear = nn.Linear(self.embed_dim, self.vocab_size)
         
         #weight sharing scheme
@@ -182,126 +196,93 @@ class Model1(nn.Module):
         x = x + self.positional_embedding(torch.arange(T, device=device))
         for block in self.blocks:
             x = block(x)
-        output = self.rmsnrom3(x)
+        output = self.rmsnorm3(x)
         output = self.lm_linear(output)
         return output
 
 
-def get_lr(it, warmup_steps, max_lr, max_steps, min_lr):
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
     if it < warmup_steps:
         return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
     if it > max_steps:
         return min_lr
+    # 3) in between, use cosine decay down to min learning rate
     decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
 
-def train(batch_size, block_size, epochs, lr, max_steps, warmup_steps, max_lr, data_root, grad_accum_steps, vocab_size, weight_decay, min_lr):
-    train_dataloader = DataLoaderLite(B=batch_size, T=block_size, split="train", data_root=data_root)
-    val_dataloader = DataLoaderLite(B=batch_size, T=block_size, split="val", data_root=data_root)
+train_dataloader = DataLoaderLite(B=batch_size, T=block_size, split="train")
+val_dataloader = DataLoaderLite(B=batch_size, T=block_size, split="val")
 
-    model = Model1(block_size=block_size, vocab_size=vocab_size, embed_dim=1024, num_heads=16, num_blocks=8, dropout=0.2).to(device)
-    if device == "mps":
-        pass
-    else:
-        model = torch.compile(model)
-        print("[green]Using compiled model[/green]")
+model = Model1(block_size=block_size, vocab_size=vocab_size, embed_dim=768, num_heads=16, num_blocks=8, dropout=0.2).to(device)
+if device == "mps":
+    pass
+else:
+    model = torch.compile(model)
 
-    param_dict = [p for p in model.parameters()]
-    param_dict = [p for p in param_dict if p.requires_grad]
-    decay_params = [p for p in param_dict if p.dim() >= 2]
-    nodecay_params = [p for p in param_dict if p.dim() < 2]
-    optim_groups = [
-        {'params': decay_params, 'weight_decay': weight_decay},
-        {'params': nodecay_params, 'weight_decay': 0.0}
-    ]
+fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+use_fused = fused_available and device == "cuda"
 
-    fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
-    use_fused = fused_available and device == "cuda"
-    optimizer = torch.optim.AdamW(optim_groups, lr=lr, fused=use_fused)
-    print("[green]Using fused AdamW optimizer[/green]")
-    loss = nn.CrossEntropyLoss()
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        loss_acum = 0.0
-        for micro_step in range(grad_accum_steps):
-            x, y = train_dataloader.next_batch()
-            x, y = x.to(device), y.to(device)
-            with torch.autocast(device_type=device, dtype=torch.bfloat16):
-                output = model(x)
-                train_loss = loss(output.view(-1, output.size(-1)), y.view(-1))
-            train_loss = train_loss / grad_accum_steps
-            loss_acum += train_loss.detach()
-            train_loss.backward()
+loss = nn.CrossEntropyLoss()
 
-        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
-        optimizer.step()
+param_dict = [p for p in model.parameters()]
+param_dict = [p for p in param_dict if p.requires_grad]
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+decay_params = [p for p in param_dict if p.dim() >= 2]
+nodecay_params = [p for p in param_dict if p.dim() < 2]
+optim_groups = [
+    {'params': decay_params, 'weight_decay': weight_decay},
+    {'params': nodecay_params, 'weight_decay': 0.0}
+]
+
+optimizer = torch.optim.AdamW(optim_groups, lr=lr, fused=use_fused)
+print("Parameter weight decay set")
+
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    loss_acum = 0.0
     
-        lr = get_lr(epoch, warmup_steps, max_lr, max_steps, min_lr)
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+    for micro_step in range(grad_accum_steps):
+        x, y = train_dataloader.next_batch()
+        x, y = x.to(device), y.to("cpu")
+        # x, y = x.to(device), y.to(device)
+        output = model(x).to("cpu")
+        # output = model(x)
+        train_loss = loss(output.view(-1, output.size(-1)), y.view(-1))
+        train_loss = train_loss.to(device) 
+        output = output.to(device)
 
-        with torch.no_grad():
-            x, y = val_dataloader.next_batch()
-            x, y = x.to(device), y.to(device)
-            with torch.autocast(device_type=device, dtype=torch.bfloat16):
-                output = model(x)
-                val_loss = loss(output.view(-1, output.size(-1)), y.view(-1))
-        
-        if os.path.exists("logs"):
-            pass
-        else:
-            print("[yellow]creating a logs directory[/yellow]") 
-            os.mkdir("logs")
+        train_loss = train_loss / grad_accum_steps
+        loss_acum += train_loss.detach()
+        train_loss.backward()
 
-        with open("logs/log.txt", 'a') as fd:
-            fd.write(f"Epoch: {epoch}| Train Loss: {loss_acum.item()} | Val Loss: {val_loss.item()} | Norm: {norm} | lr: {lr}\n")
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
 
-        print(f"[purple]Epoch[/purple]: {epoch}| [blue]Train Loss[/blue]: {loss_acum.item()} | [magenta]Val Loss[/magenta]: {val_loss.item()} | [bold cyan]Norm[/bold cyan]: {norm} | [bold turquoise4]lr[/bold turquoise4]: {lr}")
-
-    if os.path.exists("weights"):
-        pass
-    else:
-        print("[yellow]creating a weights directory[/yellow]") 
-        os.mkdir("weights")
-        
-    torch.save(model.state_dict(), "./weights/model1-edu_weights.pth")
-    torch.save(optimizer.state_dict(), "./weights/optimizer1-edu_weights.pth")
-
-@click.command()
-@click.option("--vocab_size", default=50304, help="Vocab size")
-@click.option("--block_size", default=1024, help="Block size")
-@click.option("--total_batch_size", default=524288, help="Total batch size")
-@click.option("--batch_size", default=8, help="Batch size")
-@click.option("--weight_decay", default=0.01, help="Weight decay")
-@click.option("--epochs", default=19073, help="Number of epochs")
-@click.option("--lr", default=3e-4, help="Learning rate")
-@click.option("--max_steps", default=19073, help="Maximum steps")
-@click.option("--warmup_steps", default=750, help="Warmup steps")
-@click.option("--max_lr", default=3e-4, help="Maximum learning rate")
-@click.option("--data_root", default="data", help="Path to data root")
-def set_hyperparameters(vocab_size, block_size, total_batch_size, batch_size, weight_decay, epochs, lr, max_steps, warmup_steps, max_lr, data):
-    #TODO remember additional parameter of data_root for DataLoaderLite
-    vocab_size = vocab_size
-    block_size = block_size
-    total_batch_size = total_batch_size
-    batch_size = batch_size
-    weight_decay = weight_decay
-    epochs = epochs
-    lr = lr
-    max_steps = epochs
-    warmup_steps = warmup_steps
-    max_lr = lr
-    min_lr = max_lr * 0.1
-    grad_accum_steps = total_batch_size // (batch_size * block_size)
-
-    if total_batch_size % (batch_size * block_size) == 0:
-        pass
-    else:
-        log.error("total_batch_size must be divisble by B * T")
+    output = output.to(device)
     
-    train(batch_size, block_size, epochs, lr, max_steps, warmup_steps, max_lr, data, grad_accum_steps, vocab_size, weight_decay, min_lr)
+    # for param in model.parameters():
+        # param.grad = None
+
+    optimizer.step()
     
-    print(f"Grad accum steps: {grad_accum_steps} | Vocab size: {vocab_size} | Block size: {block_size} | Total batch size: {total_batch_size} | Batch size: {batch_size} | Weight decay: {weight_decay} | Number of epochs: {epochs} | Learning rate: {lr} | Maximum steps: {max_steps} | Warmup steps: {warmup_steps} | Maximum learning rate: {max_lr}")
-    
+    lr = get_lr(epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    with torch.no_grad():
+        x, y = val_dataloader.next_batch()
+        x, y = x.to(device), y.to("cpu")
+        output = model(x)
+        output = output.to("cpu")
+        val_loss = loss(output.view(-1, output.size(-1)), y.view(-1))
+
+    print_statement = f"Epoch: {epoch}| Train Loss: {loss_acum.item()} | Val Loss: {val_loss.item()} | Norm: {norm} | lr: {lr}"
+
+    with open("log.txt", 'a') as fd:
+        fd.write(f"{print_statement}\n")
+
+    print(print_statement)
